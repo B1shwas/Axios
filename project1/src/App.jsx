1771238a102df62da08982e185677b04334633cf
Bishwash <bishwash312@gmail.com>
2024-01-12 11:42:11 +0545

    components folder managed

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CardProvider } from "./context/Cardcontext";
 import Card from "./components/Card/Card";
-import CardDescription from "./components/Card/CardDescription/CardDescription";
+import CardDescription from "./components/CardDescription/CardDescription";
 import "./App.css";
 
 const App = () => {
--- project1/src/components/CardDescription/CardDescription.jsx
+++ project1/src/components/CardDescription/CardDescription.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
-import CardContext from "../../../context/Cardcontext";
+import CardContext from "../../context/Cardcontext";
 import "./Description.css";
 import { IoAddCircleSharp } from "react-icons/io5";
 import { FaEdit } from "react-icons/fa";
